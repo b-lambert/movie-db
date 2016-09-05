d3dbdb6aa492ce46a55f5a3e1198aac232cf3f02
b-lambert <benjaminhlambert@gmail.com>
2016-09-05 12:45:47 -0700

    fix search results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,10 +2,14 @@ class MoviesController < ApplicationController
 
   def index
     # TODO cleanup
-    results = HTTParty.post("http://api.themoviedb.org/3/movie/popular", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"})["results"]
+    if params[:title].present?
+      results = HTTParty.get("http://api.themoviedb.org/3/search/movie", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13", query: params[:title]})["results"]
+    else
+      results = HTTParty.post("http://api.themoviedb.org/3/movie/popular", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"})["results"]
+    end
     p results
-    @titles = results.each_with_object([]) do |result, titles_list|
-      titles_list << {title: result["title"], id: result["id"]}
+    @movies = results.each_with_object([]) do |result, movie_list|
+      movie_list << {title: result["title"], id: result["id"]}
     end
   end
 
@@ -14,12 +18,13 @@ class MoviesController < ApplicationController
     response = HTTParty.get("http://api.themoviedb.org/3/movie/#{id}", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"}).parsed_response
     p response
     @title = response["title"]
-    @genres = response["genres"].map{|x| x["name"]}.join(",")
+    @genres = response["genres"].map{|x| x["name"]}.join(", ")
     @overview = response["overview"]
-    @production_companies = response["production_companies"].map{|x| x["name"]}.join(",")
+    @production_companies = response["production_companies"].map{|x| x["name"]}.join(", ")
     @release_date = response["release_date"]
     @revenue = response["revenue"]
     @tagline = response["tagline"]
+    @poster_path = response["poster_path"]
   end
 
 end
