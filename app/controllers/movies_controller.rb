e6691b1a06501d8bd9f0722cbb2c2fb7d5f15a2f
b-lambert <benjaminhlambert@gmail.com>
2016-09-05 14:55:26 -0700

    error handling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,15 @@
 class MoviesController < ApplicationController
 
   def index
-    # TODO cleanup
+    if params[:error].present?
+      @error = true
+    end
     if params[:title].present?
       results = HTTParty.get("http://api.themoviedb.org/3/search/movie", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13", query: params[:title]})["results"]
     else
+      res = HTTParty.post("http://api.themoviedb.org/3/movie/popular", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"})
       results = HTTParty.post("http://api.themoviedb.org/3/movie/popular", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"})["results"]
     end
-    p results
     @movies = results.each_with_object([]) do |result, movie_list|
       movie_list << {title: result["title"], id: result["id"]}
     end
@@ -15,15 +17,19 @@ class MoviesController < ApplicationController
 
   def show
     id = params[:id]
-    response = HTTParty.get("http://api.themoviedb.org/3/movie/#{id}", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"}).parsed_response
-    @title = response["title"]
-    @genres = response["genres"].map{|x| x["name"]}.join(", ")
-    @overview = response["overview"]
-    @production_companies = response["production_companies"].map{|x| x["name"]}.join(", ")
-    @release_date = response["release_date"]
-    @revenue = response["revenue"]
-    @tagline = response["tagline"]
-    @poster_path = response["poster_path"]
+    response = HTTParty.get("http://api.themoviedb.org/3/movie/#{id}", body: {api_key: "c5682edbb47129ba8c07d25e6d13db13"})
+    if response.code != 200
+      redirect_to controller: 'movies', action: 'index', error: true
+    else
+      @title = response["title"]
+      @genres = response["genres"].map{|x| x["name"]}.join(", ")
+      @overview = response["overview"]
+      @production_companies = response["production_companies"].map{|x| x["name"]}.join(", ")
+      @release_date = response["release_date"]
+      @revenue = response["revenue"]
+      @tagline = response["tagline"]
+      @poster_path = response["poster_path"]
+    end
   end
 
 end
